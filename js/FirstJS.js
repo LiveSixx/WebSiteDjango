113f9374f99d462aa05b47cfbec2f694c99fa57c
Васильеы Артём <tema556639@mail.ru>
2020-02-17 19:13:34 +0300

    First Test Save

--- js/FirstJS.js
+++ js/FirstJS.js
@@ -1,3 +1,3 @@
-var perem = 12, stringas = 'lol', perem1 = 12, perem3 = Math.sqrt(125);
+var perem = 12, stringas = 'lol', perem1 = 13, perem3 = Math.sqrt(125);
 var res = perem3%2;
 document.write(res);
