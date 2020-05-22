2ef2b48ea46451bb5e53d67a29b1d2c7302c5df2
Artem <34215030+LiveSixx@users.noreply.github.com>
2020-05-23 01:28:29 +0300

    header done 80%

--- playGroundSite/siteHomePage/views.py
+++ playGroundSite/siteHomePage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'siteHomePage/homePage.html')
+
+def homepage(request):
+    return render(request, 'siteHomePage/homePage2.html')
